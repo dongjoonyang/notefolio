562751a12b25d8ce74a764a4826c7c14c77b8c90
dongjoonyang <motokrzr11@gmail.com>
2026-01-24 22:59:42 +0900

    add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 import { Toaster } from 'sonner';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -25,6 +26,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             duration={1500} // 💡 1.5초 후 자동으로 닫힘
           />
         </ThemeProvider>
+        
+        <SpeedInsights />
       </body>
     </html>
   );
