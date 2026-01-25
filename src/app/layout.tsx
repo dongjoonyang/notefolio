0633514bae82c31c857df7e38381520fe0ca66e8
dongjoonyang <motokrzr11@gmail.com>
2026-01-25 16:23:12 +0900

    og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,35 @@ import "./globals.css";
 import { Toaster } from 'sonner';
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Plug Log 포트폴리오',
+  description: 'Plug Log 포트폴리오와 프로젝트를 확인해보세요.',
+  openGraph: {
+    title: 'Plug Log',
+    description: 'Plug Log의 포트폴리오와 프로젝트를 확인해보세요.',
+    url: 'https://www.pluglog.com/', 
+    siteName: 'Plug Log',
+    images: [
+      {
+        url: '/logo.svg',
+        width: 1200,
+        height: 630,
+        alt: 'Plug Log 로고',
+      },
+    ],
+    locale: 'ko_KR',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Plug Log',
+    description: 'Plug Log 포트폴리오와 프로젝트를 확인해보세요.',
+    images: ['/logo.svg'],
+  },
+}
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko" suppressHydrationWarning>
